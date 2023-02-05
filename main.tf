48197111dcde1d3450ef9ccf0fd63d4bd89b2b80
Okotie Asiekunmoritse <108562214+OkotieAsiekunmoritse@users.noreply.github.com>
2023-02-05 03:13:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -387,3 +387,35 @@ resource "aws_lb_target_group_attachment" "Miniproject-target-group-attachment3"
   port             = 80
 
   }
+
+
+# Create HTTPS listener for ALB
+resource "aws_lb_listener" "https_lb_listener" {
+  load_balancer_arn = aws_lb.Miniproject-load-balancer.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.ssl_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.Miniproject-target-group.arn
+  }
+}
+
+# Create a redirect of HTTP  to HTTPS
+resource "aws_lb_listener" "http_to_https" {
+  load_balancer_arn = aws_lb.Miniproject-load-balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
