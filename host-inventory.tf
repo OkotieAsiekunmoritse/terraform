1a451676b42dd76d3678685c338fff742b1d1b95
Okotie Asiekunmoritse <108562214+OkotieAsiekunmoritse@users.noreply.github.com>
2023-02-05 03:58:21 +0100

    Add files via upload

---
+++ host-inventory.tf
@@ -0,0 +1,12 @@
+#3.80.100.100
+#34.201.221.137
+#44.203.28.249
+
+minip-1 ansible_host=3.80.100.100 ansible_ssh_user=ubuntu ansible_ssh_private_key=project_key.pem
+minip-2 ansible_host=34.201.221.137 ansible_ssh_user=ubuntu ansible_ssh_private_key=project_key.pem
+minip-3 ansible_host=44.203.28.249 ansible_ssh_user=ubuntu ansible_ssh_private_key=project_key.pem
+
+[apache]
+minip-1
+minip-2
+minip-3
