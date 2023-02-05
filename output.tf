1c258c80f02aab665f3a4846026eed77b1d0df46
Okotie Asiekunmoritse <108562214+OkotieAsiekunmoritse@users.noreply.github.com>
2023-02-05 04:03:29 +0100

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,13 @@
+# Create load balancer outputs
+
+output "elb_target_groupr_arn" {
+  value = aws_lb_target_group.Miniproject-target-group.arn
+}
+
+output "elb_load_balancer_dns_name" {
+  value = aws_lb.Miniproject-load-balancer.dns_name
+}
+
+output "elastic_load_balancer_zone_id" {
+  value = aws_lb.Miniproject-load-balancer.zone_id
+}
