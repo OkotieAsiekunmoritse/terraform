9ac678c28a1cbe884f3e1fcb719db751cc123570
Okotie Asiekunmoritse <108562214+OkotieAsiekunmoritse@users.noreply.github.com>
2023-02-05 04:25:20 +0100

    Update host-inventory.tf

--- host-inventory.tf
+++ host-inventory.tf
@@ -1,7 +1,8 @@
-#3.80.100.100
-#34.201.221.137
-#44.203.28.249
+3.80.100.100
+34.201.221.137
+44.203.28.249
 
+#Reassign the ip ansible public_ip addresses to new names and features 
 minip-1 ansible_host=3.80.100.100 ansible_ssh_user=ubuntu ansible_ssh_private_key=project_key.pem
 minip-2 ansible_host=34.201.221.137 ansible_ssh_user=ubuntu ansible_ssh_private_key=project_key.pem
 minip-3 ansible_host=44.203.28.249 ansible_ssh_user=ubuntu ansible_ssh_private_key=project_key.pem
